a08491717ff916b795fbe8219bf1c90a42c0606d
David <andrianarivo.david@gmail.com>
2025-05-31 22:38:23 +0300

    refactor

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ImportController } from './import/import.controller';
+import { TranslationController } from './translation/translation.controller';
 
 @Module({
   imports: [],
   controllers: [AppController, ImportController],
   providers: [AppService],
+  controllers: [AppController, TranslationController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
