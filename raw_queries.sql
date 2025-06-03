c92024a0e9ca14e7582115fd09b7dd2202bb78f3
David <andrianarivo.david@gmail.com>
2025-06-03 14:16:50 +0300

    supporting files

--- raw_queries.sql
+++ raw_queries.sql
@@ -20,12 +20,17 @@ TRUNCATE translation_files RESTART IDENTITY CASCADE;
 TRUNCATE translations RESTART IDENTITY CASCADE;
 TRUNCATE contents RESTART IDENTITY CASCADE;
 
-SELECT s1.key,
+SELECT s1.translation_id AS "translation%fr-FR%id",
+	s2.translation_id AS "translation%en-US%id",
+	s1.id AS "content%fr-FR%id",
+	s2.id AS "content%en-US%id",
+	s1.key,
 	s1.value AS "fr-FR",
-	s2.value AS "en-US",
-	s3.value AS "en-US"
+	s2.value AS "en-US"
 FROM (
-	SELECT c.key,
+	SELECT t.id AS translation_id,
+		c.id,
+		c.key,
 		c.value,
 		t.name AS lang
 	FROM translations t 
@@ -34,22 +39,18 @@ FROM (
 	WHERE t.name = 'fr-FR'
 ) s1
 LEFT OUTER JOIN (
-	SELECT c.key,
+	SELECT t.id AS translation_id,
+		c.id,
+		c.key,
 		c.value,
 		t.name AS lang
 	FROM translations t 
-	JOIN contents c 
+	JOIN contents c
 	ON c.translation_id = t.id
 	WHERE t.name = 'en-US'
 ) s2
-ON s1.key = s2.key
-LEFT OUTER JOIN (
-	SELECT c.key,
-		c.value,
-		t.name as language
-	FROM translations t 
-	JOIN contents c 
-	ON c.translation_id = t.id
-	WHERE t.name = 'en-US'
-) s3
-ON s1.key = s3.key;
+ON s1.key = s2.key;
+
+SELECT * FROM translation_files;
+
+DELETE FROM translation_files WHERE id in [];
